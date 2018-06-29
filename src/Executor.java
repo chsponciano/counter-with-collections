ca02c8dfe7a651721eaf925b259db2b08eb8b58d
vinicius <vinicius.lds.br@gmail.com>
2018-06-29 20:59:15 -0300

    Alteração tela

--- src/Executor.java
+++ src/Executor.java
@@ -1,16 +1,9 @@
-//Testes
-
+/**
+ * @author Vinícius Luis da Silva && Carlos Henrique Ponciano da Silva
+ */
 public class Executor {
 
     public static void main(String[] args) {
-        /*String frase = "Eu estou escrevendo um programa, mas eu não tenho um programa para escrever";
-        ContadorMap m = new ContadorMap();
-        ContadorCollection c = new ContadorCollection();
-        
-        System.out.println(c.contar(frase));        
-        System.out.println(m.contar(frase));*/
-        
         new Apresentacao().setVisible(true);
-
     }
 }
